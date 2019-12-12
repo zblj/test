e492a4e1a6cc142fb4f592afbbbf19765b0935d6
lupo <lupo@git.com>
2019-12-12 18:29:48 +0100

    added login function to app.py

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
 	return render_template('main.html')
+	
+@app.route("/login/")
+def hello():
+	return render_template('login.html')
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=5000, debug=True)
